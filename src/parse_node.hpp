e477856c37da041bc9934fe9d87ea4a41c3a6611
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2014-09-02 23:58:45 +0200

    fix curly_list, implement square_list and incomplete parse_node

--- src/parse_node.hpp
+++ src/parse_node.hpp
@@ -3,6 +3,9 @@
 
 #include "symbol.hpp"
 #include "parse_error.hpp"
+#include "parse_literal.hpp"
+#include "parse_reference.hpp"
+#include "whitespace.hpp"
 
 #include <boost/optional.hpp>
 
@@ -65,8 +68,12 @@ boost::optional<symbol> parse_curly_list(State& state)
         state.pop_front();
         typename symbol::list result;
         boost::optional<symbol> semicolon_list;
+        whitespace(state);
         while( (semicolon_list = parse_semicolon_list(state)) )
+        {
             result.push_back(std::move(*semicolon_list));
+            whitespace(state);
+        }
         
         if(state.empty() || state.front() != '}')
             throw parse_error();
@@ -76,6 +83,52 @@ boost::optional<symbol> parse_curly_list(State& state)
             return symbol{begin, state.location(), std::move(result)};
         }
     }
+}
+
+template <class State>
+boost::optional<symbol> parse_square_list(State& state)
+{
+    if(state.empty() || state.front() != '[')
+        return boost::none;
+    else
+    {
+        source_location begin = state.location();
+
+        symbol::list result;
+        do
+        {
+            state.pop_front(); // the first time, this will pop '[', afterwards ','
+            whitespace(state);
+            source_location before_nodes = state.location();
+            
+            symbol::list current_list = parse_nodes(state);
+            result.push_back(symbol{before_nodes, state.location(), std::move(current_list)});
+        } while(!state.empty() && state.front() == ',');
+        
+        if(state.empty() || state.front() != ']')
+            throw parse_error();
+        else
+        {
+            state.pop_front();
+            return symbol{begin, state.location(), std::move(result)};
+        }
+    }
+}
+
+template <class State>
+boost::optional<symbol> parse_node(State& state)
+{
+    boost::optional<symbol> result;
+    if( (result = parse_literal(state)) )
+        return result;
+    else if( (result = parse_reference(state)) )
+        return result;
+    else if( (result = parse_curly_list(state)) )
+        return result;
+    else if( (result = parse_square_list(state)) )
+        return result;
+    else
+        return boost::none;
 }
 
 #endif
--- test/parse_node.cpp
+++ test/parse_node.cpp
@@ -9,7 +9,63 @@ int main()
 {
     typedef parse_state<string::iterator> state;
     {
+        string str = "abcd efg";
+        state state(str.begin(), str.end());
+        symbol result = *parse_node(state);
+        assert(boost::get<symbol::reference>(result.content).identifier == "abcd");
+        assert(remaining(state) == " efg");
+    }
+    {
+        string str = "\"abcd\" efg";
+        state state(str.begin(), str.end());
+        symbol result = *parse_node(state);
+        assert(boost::get<symbol::literal>(result.content) == "abcd");
+        assert(remaining(state) == " efg");
+    }
+    {
+        string str = "{ abc; \"fff\" def; ; } efg";
+        state state(str.begin(), str.end());
+        symbol result = *parse_node(state);
+        assert(remaining(state) == " efg");
 
+        symbol::list content = boost::get<symbol::list>(result.content);
+        assert(content.size() == 3);
+
+        symbol::list first_content = boost::get<symbol::list>(content[0].content);
+        symbol::list second_content = boost::get<symbol::list>(content[1].content);
+        symbol::list third_content = boost::get<symbol::list>(content[2].content);
+        assert(first_content.size() == 1);
+        assert(second_content.size() == 2);
+        assert(third_content.size() == 0);
+        assert(boost::get<symbol::reference>(first_content[0].content).identifier == "abc");
+        
+        assert(boost::get<symbol::literal>(second_content[0].content) == "fff");
+        assert(boost::get<symbol::reference>(second_content[1].content).identifier == "def");
 
     }
+    {
+        string str = "[ef ak, [a ], [ ] ,]";
+        state state(str.begin(), str.end());
+        symbol result = *parse_node(state);
+        assert(remaining(state) == "");
+
+        symbol::list content = boost::get<symbol::list>(result.content);
+        assert(content.size() == 4);
+
+        symbol::list first_content = boost::get<symbol::list>(content[0].content);
+        symbol::list second_content = boost::get<symbol::list>(content[1].content);
+        symbol::list third_content = boost::get<symbol::list>(content[2].content);
+        symbol::list fourth_content = boost::get<symbol::list>(content[3].content);
+        assert(first_content.size() == 2);
+        assert(second_content.size() == 1);
+        assert(third_content.size() == 1);
+        assert(fourth_content.size() == 0);
+        assert(boost::get<symbol::reference>(first_content[0].content).identifier == "ef");
+        assert(boost::get<symbol::reference>(first_content[1].content).identifier == "ak");
+        
+        assert(boost::get<symbol::list>(second_content[0].content).size() == 1);
+        assert(boost::get<symbol::list>(third_content[0].content).size() == 1);
+    }
+
 }
+
