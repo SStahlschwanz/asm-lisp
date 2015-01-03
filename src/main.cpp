a6d991dda0b4e7820d9b8a2dd0641a97688992bb
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2015-01-03 21:59:00 +0100

    remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,4 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    if(argc != 2)
-    {
-        cerr << "usage: " << argv[0] << " [file]" << endl;
-        return 1;
-    }
-    else
-    {
-        try
-        {
-            symbol::list parsed_module = parse_module(argv[1]);
-            vector<string> imports = required_modules(parsed_module);
-            for(auto s : imports)
-                cout << s << endl;
-            return 0;
-        }
-        catch(const parse_error& exc)
-        {
-            cerr << exc.what() << endl;
-            return 1;
-        }
-        catch(const exception& exc)
-        {
-            cerr << "error: " << exc.what() << endl;
-            return 2;
-        }
-    }
 }
