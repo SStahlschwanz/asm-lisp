850cddb553cbce7d6f7c6d6a534fa1fdbf5d67f9
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2015-02-07 23:13:36 +0100

    add unit test for module

--- test/module.cpp
+++ test/module.cpp
@@ -3,28 +3,77 @@
 #include <cassert>
 #include <fstream>
 #include <iterator>
+#include <sstream>
+#include <string>
 
 using namespace std;
 using namespace symbol_building;
 
+stringstream source1(
+R"(
+{};
+
+"asdf" asdfa f;
+import fff;
+ff;
+ff;
+import aa
+bb;
+import aa;
+)");
+
+symbol::list source1_syntax = 
+{
+    list(list()),
+    list(lit("asdf"), sref("asdfa"), sref("f")),
+    list(sref("import"), sref("fff")),
+    list(sref("ff")),
+    list(sref("ff")),
+    list(sref("import"), sref("aa"), sref("bb")),
+    list(sref("import"), sref("aa"))
+};
+
+vector<string> source1_imports = {"fff", "aa", "bb"};
+
+
+stringstream source2(
+R"(def a {};
+def b "asdf" {};
+def c x;
+def d b;
+)");
+
+symbol::list source2_syntax =
+{
+    list(sref("def"), sref("a"), list()),
+    list(sref("def"), sref("b"), lit("asdf"), list()),
+    list(sref("def"), sref("c"), sref("x")),
+    list(sref("def"), sref("d"), sref("b"))
+};
+
+vector<string> source2_imports = {};
+
+module verify(stringstream& source, const symbol::list& syntax_tree, vector<string> imports)
+{
+    module mod = read_module(source);
+    assert(mod.syntax_tree == syntax_tree);
+    sort(imports.begin(), imports.end());
+    assert(mod.required_modules == imports);
+    return mod;
+}
+
 int main()
 {
-    ifstream file("test-res/module1.al");
-    module mod = read_module(file);
-    symbol::list syntax_tree =
-    {
-        list(list()),
-        list(lit("asdf"), sref("asdfa"), sref("f")),
-        list(sref("import"), sref("fff")),
-        list(sref("ff")),
-        list(sref("ff")),
-        list(sref("import"), sref("aa"), sref("bb")),
-        list(sref("import"), sref("aa"))
-    };
-    assert(mod.syntax_tree == syntax_tree);
-
-    vector<string> required_modules = {"fff", "aa", "bb"};
-    std::sort(required_modules.begin(), required_modules.end());
-    
-    assert(mod.required_modules == required_modules);
+    verify(source1, source1_syntax, source1_imports);
+    module m2 = verify(source2, source2_syntax, source2_imports);
+    dispatch_and_eval(m2, {});
+    symbol a_def = list() ;
+    symbol b_def = list(lit("asdf"),  list());
+    symbol c_def = sref("x");
+    symbol d_def = sref("b", &b_def);
+    assert(m2.defined_symbols.size() == 4);
+    assert(a_def == m2.defined_symbols["a"]);
+    assert(b_def == m2.defined_symbols["b"]);
+    assert(c_def == m2.defined_symbols["c"]);
+    assert(d_def == m2.defined_symbols["d"]);
 }
