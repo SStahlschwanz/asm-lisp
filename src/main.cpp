87bb4a292522948f4bdd23c79f9ac71eb1ddee8e
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2015-02-17 22:47:33 +0100

    add very simple parsing from command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
-#include "module.hpp"
+# include "module.hpp"
+#include "parse_state.hpp"
 
 #include <iostream>
-#include <algorithm>
+#include <iterator>
+
 
 using namespace std;
 
-//int main(int argc, char** argv)
 int main()
 {
+    istream_iterator<char> begin{std::cin};
+    istream_iterator<char> end{};
+    
+    parse_state<istream_iterator<char>> state{begin, end, "command line"};
+    
+    try
+    {
+        parse_file(state);
+    } catch(const parse_exception& exc)
+    {
+        cerr << "command line:" << exc.pos.line << ":" << exc.pos.line_position 
+                << ": error: " << parse_error_strings[(unsigned int)exc.error] << endl;
+    }
 }
