b57ebba331df93c5f81cf2c1c8de21f81217be60
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2015-02-21 16:04:47 +0100

    new symbol implementation (and api) - doesn't compile

--- src/symbol.hpp
+++ src/symbol.hpp
@@ -5,92 +5,765 @@
 
 #include <boost/variant.hpp>
 
-#include <functional>
 #include <vector>
+#include <exception>
+#include <initializer_list>
+
+
+class none_symbol;
+class lit_symbol;
+class ref_symbol;
+class list_symbol;
+
+class symbol_impl;
 
 class symbol
 {
 public:
-    typedef std::string literal;
-    struct reference
+    enum type_value
     {
-        reference(std::string id = std::string(), const symbol* ref = nullptr)
-          : identifier(std::move(id)),
-            refered(ref)
-        {}
-        std::string identifier;
-        const symbol* refered;
-
-        bool operator==(const reference& that) const
-        {
-            if(identifier != that.identifier)
-                return false;
-
-            if(refered != nullptr && that.refered != nullptr)
-                return *refered == *that.refered;
-            else
-                return refered == nullptr && that.refered == nullptr;
-        }
-    };
-    typedef std::vector<symbol> list;
-    struct macro
-    {
-        std::function<symbol (symbol::list param)> f;
-        bool operator==(const macro&) const
-        {
-            return true;
-        }
+        NONE,
+        LITERAL,
+        REFERENCE,
+        LIST
     };
     
-    typedef boost::variant<literal, reference, list, macro> content_type;
-    content_type content;
+    type_value type() const;
+    const symbol_source& source() const;
+    void source(const symbol_source& new_source);
     
-    typedef boost::variant<boost::blank, source_range> source_type;
-    source_type source;
-    
-    literal* cast_literal()
+    bool is_none() const;
+    none_symbol& cast_none();
+    const none_symbol& cast_none() const
     {
-        return boost::get<literal>(&content);
-    }
-    reference* cast_reference()
-    {
-        return boost::get<reference>(&content);
-    }
-    list* cast_list()
-    {
-        return boost::get<list>(&content);
-    }
-    macro* cast_macro()
-    {
-        return boost::get<macro>(&content);
+        return const_cast<symbol*>(this)->cast_none();
     }
     
-    const literal* cast_literal() const
+    lit_symbol& lit();
+    const lit_symbol& lit() const
     {
-        return boost::get<literal>(&content);
+        return const_cast<symbol*>(this)->lit();
     }
-    const reference* cast_reference() const
+    lit_symbol& lit_else(const std::exception& exc)
     {
-        return boost::get<reference>(&content);
+        if(type() == LITERAL)
+            return lit();
+        else
+            throw exc;
     }
-    const list* cast_list() const
+    const lit_symbol& lit_else(const std::exception& exc) const
     {
-        return boost::get<list>(&content);
-    }
-    const macro* cast_macro() const
-    {
-        return boost::get<macro>(&content);
+        return const_cast<symbol*>(this)->lit_else(exc);
     }
 
-    bool operator==(const symbol& that) const
+    ref_symbol& ref();
+    const ref_symbol& ref() const
     {
-        return content == that.content;
+        return const_cast<symbol*>(this)->ref();
     }
-    bool operator!=(const symbol& that) const
+    ref_symbol& ref_else(const std::exception& exc)
+    {
+        if(type() == REFERENCE)
+            return ref();
+        else
+            throw exc;
+    }
+    const ref_symbol& ref_else(const std::exception& exc) const
+    {
+        return const_cast<symbol*>(this)->ref_else(exc);
+    }
+
+    list_symbol& list();
+    const list_symbol& list() const
+    {
+        return const_cast<symbol*>(this)->list();
+    }
+    list_symbol& list_else(const std::exception& exc)
+    {
+        if(type() == LIST)
+            return list();
+        else
+            throw exc;
+    }
+    const list_symbol& list_else(const std::exception& exc) const
+    {
+        return const_cast<symbol*>(this)->list_else(exc);
+    }
+    
+    template<class Functor>
+    void visit(Functor&& f)
+    {
+        switch(type())
+        {
+        case NONE:
+            assert(type() != NONE); // crashes always
+            break;
+        case LITERAL:
+            f(lit());
+            break;
+        case REFERENCE:
+            f(ref());
+            break;
+        case LIST:
+            f(list());
+            break;
+        default:
+            assert(false);
+            break;
+        }
+    }
+    template<class Functor>
+    void visit(Functor&& f) const
+    {
+        const_cast<symbol*>(this)->visit([&](auto& obj)
+        {
+            typedef typename std::decay<decltype(obj)>::type T;
+            f(const_cast<const T&>(obj));
+        });
+    }
+    template<class Functor>
+    void visit_none(Functor&& f)
+    {
+        switch(type())
+        {
+        case NONE:
+            f(cast_none());
+            break;
+        case LITERAL:
+            f(lit());
+            break;
+        case REFERENCE:
+            f(ref());
+            break;
+        case LIST:
+            f(list());
+            break;
+        default:
+            assert(false);
+            break;
+        }
+    }
+    template<class Functor>
+    void visit_none(Functor&& f) const
+    {
+        const_cast<symbol*>(this)->visit_none([&](auto& obj)
+        {
+            typedef typename std::decay<decltype(obj)>::type T;
+            f(const_cast<const T&>(obj));
+        });
+    }
+private:
+    friend class any_symbol;
+    friend class symbol_impl;
+    ~symbol()
+    {}
+
+    symbol_impl& impl();
+    const symbol_impl& impl() const;
+};
+
+class symbol_impl
+  : public symbol
+{
+private:
+    friend class symbol;
+    friend class none_symbol;
+    friend class lit_symbol;
+    friend class ref_symbol;
+    friend class list_symbol;
+
+    ~symbol_impl()
+    {}
+    type_value type_id;
+    symbol_source source_value;
+};
+
+class none_symbol
+  : public symbol_impl
+{
+public:
+    none_symbol()
+    {
+        type_id = NONE;
+    }
+
+    bool operator==(const none_symbol&) const
+    {
+        return true;
+    }
+    bool operator!=(const none_symbol&) const
+    {
+        return false;
+    }
+};
+
+class lit_symbol
+  : public symbol_impl
+{
+public:
+    lit_symbol(std::string str)
+      : s(std::move(str))
+    {
+        type_id = LITERAL;
+    }
+    lit_symbol(const char* str)
+      : lit_symbol(std::string{str})
+    {}
+    lit_symbol()
+      : lit_symbol("")
+    {}
+    
+    lit_symbol(const lit_symbol&) = default;
+    lit_symbol(lit_symbol&& that) noexcept
+      : lit_symbol(std::move(that.s))
+    {}
+
+    lit_symbol& operator=(lit_symbol that)
+    {
+        std::swap(s, that.s);
+        return *this;
+    }
+    
+    typedef std::string::iterator iterator;
+    typedef std::string::const_iterator const_iterator;
+    iterator begin()
+    {
+        return s.begin();
+    }
+    iterator end()
+    {
+        return s.end();
+    }
+    const_iterator begin() const
+    {
+        return s.begin();
+    }
+    const_iterator end() const
+    {
+        return s.end();
+    }
+    char& operator[](size_t i)
+    {
+        return s[i];
+    }
+    const char& operator[](size_t i) const
+    {
+        return s[i];
+    }
+    void push_back(char c)
+    {
+        s.push_back(c);
+    }
+    void pop_back()
+    {
+        s.pop_back();
+    }
+
+    bool operator==(const lit_symbol& that) const
+    {
+        return s == that.s;
+    }
+    bool operator!=(const lit_symbol& that) const
     {
         return !(*this == that);
     }
+private:
+    std::string s;
 };
+static_assert(std::is_nothrow_move_constructible<lit_symbol>::value, "");
+
+
+class ref_symbol
+  : public symbol_impl
+{
+public:
+    ref_symbol(std::string str, const symbol* reference)
+      : s(std::move(str))
+    {
+        type_id = REFERENCE;
+        r = reference;
+    }
+    ref_symbol()
+      : ref_symbol("", 0)
+    {}
+
+    ref_symbol(const ref_symbol&) = default;
+    ref_symbol(ref_symbol&& that) noexcept
+      : ref_symbol(std::move(that.s), that.r)
+    {}
+
+    explicit ref_symbol(std::string str)
+      : ref_symbol(std::move(str), 0)
+    {}
+    explicit ref_symbol(const char* str)
+      : ref_symbol(std::string{str})
+    {}
+    
+    ref_symbol& operator=(ref_symbol that)
+    {
+        std::swap(s, that.s);
+        std::swap(r, that.r);
+        return *this;
+    }
+    
+    typedef std::string::iterator iterator;
+    typedef std::string::const_iterator const_iterator;
+    iterator begin()
+    {
+        return s.begin();
+    }
+    iterator end()
+    {
+        return s.end();
+    }
+    const_iterator begin() const
+    {
+        return s.begin();
+    }
+    const_iterator end() const
+    {
+        return s.end();
+    }
+    char& operator[](size_t i)
+    {
+        return s[i];
+    }
+    const char& operator[](size_t i) const
+    {
+        return s[i];
+    }
+    void push_back(char c)
+    {
+        s.push_back(c);
+    }
+    void pop_back()
+    {
+        s.pop_back();
+    }
+
+    const symbol* refered() const
+    {
+        return r;
+    }
+    void refered(const symbol* new_reference)
+    {
+        r = new_reference;
+    }
+
+    bool operator==(const ref_symbol& that) const
+    {
+        return s == that.s && r == that.r;
+    }
+    bool operator!=(const ref_symbol& that) const
+    {
+        return !(*this == that);
+    }
+private:
+    std::string s;
+    const symbol* r;
+};
+static_assert(std::is_nothrow_move_constructible<ref_symbol>::value, "");
+
+ 
+class any_symbol;
+class list_symbol
+  : public symbol_impl
+{
+public:
+    list_symbol(std::vector<any_symbol> vec)
+      : v(std::move(vec))
+    {
+        type_id = LIST;
+    }
+    list_symbol()
+      : list_symbol(std::vector<any_symbol>{})
+    {}
+    list_symbol(std::initializer_list<any_symbol> l);
+    
+    list_symbol(const list_symbol&) = default;
+    list_symbol(list_symbol&& that) noexcept
+      : list_symbol(std::move(that.v))
+    {}
+
+    list_symbol& operator=(list_symbol that)
+    {
+        std::swap(v, that.v);
+        return *this;
+    }
+
+    typedef std::vector<any_symbol>::iterator iterator;
+    typedef std::vector<any_symbol>::const_iterator const_iterator;
+    iterator begin()
+    {
+        return v.begin();
+    }
+    iterator end()
+    {
+        return v.end();
+    }
+    const_iterator begin() const
+    {
+        return v.begin();
+    }
+    const_iterator end() const
+    {
+        return v.end();
+    }
+    any_symbol& operator[](size_t i);
+    const any_symbol& operator[](size_t i) const;
+    void push_back(symbol&& s);
+    void push_back(const symbol& s);
+    void pop_back()
+    {
+        v.pop_back();
+    }
+
+    bool operator==(const list_symbol& that) const
+    {
+        return v == that.v;
+    }
+    bool operator!=(const list_symbol& that) const
+    {
+        return !(*this == that);
+    }
+
+private:
+    std::vector<any_symbol> v;
+};
+static_assert(std::is_nothrow_move_constructible<list_symbol>::value, "");
+
+
+
+inline symbol_impl& symbol::impl()
+{
+    return *static_cast<symbol_impl*>(this);
+}
+inline const symbol_impl& symbol::impl() const
+{
+    return *static_cast<const symbol_impl*>(this);
+}
+inline symbol::type_value symbol::type() const
+{
+    return impl().type_id;
+}
+inline const symbol_source& symbol::source() const
+{
+    return impl().source_value;
+}
+inline void symbol::source(const symbol_source& new_source)
+{
+    impl().source_value = new_source;
+}
+inline bool symbol::is_none() const
+{
+    return impl().type_id == NONE;
+}
+inline none_symbol& symbol::cast_none()
+{
+    assert(impl().type_id == NONE);
+    return *static_cast<none_symbol*>(this);
+}
+inline lit_symbol& symbol::lit()
+{
+    assert(impl().type_id == LITERAL);
+    return *static_cast<lit_symbol*>(this);
+}
+inline ref_symbol& symbol::ref()
+{
+    assert(impl().type_id == REFERENCE);
+    return *static_cast<ref_symbol*>(this);
+}
+inline list_symbol& symbol::list()
+{
+    assert(impl().type_id == LIST);
+    return *static_cast<list_symbol*>(this);
+}
+
+inline bool operator==(const symbol& lhs, const symbol& rhs)
+{
+    if(lhs.type() != rhs.type())
+        return false;
+    
+    switch(lhs.type())
+    {
+    case symbol::NONE:
+        return lhs.cast_none() == rhs.cast_none();
+    case symbol::LITERAL:
+        return lhs.lit() == rhs.lit();
+    case symbol::REFERENCE:
+        return lhs.ref() == rhs.ref();
+    case symbol::LIST:
+        return lhs.list() == rhs.list();
+    default:
+        assert(false);
+    }
+}
+inline bool operator!=(const symbol& lhs, const symbol& rhs)
+{
+    return !(lhs == rhs);
+}
+
+
+class any_symbol
+{
+public:
+    any_symbol()
+      : content(none_symbol{})
+    {}
+    any_symbol(lit_symbol literal)
+      : content(std::move(literal))
+    {}
+    any_symbol(ref_symbol reference)
+      : content(std::move(reference))
+    {}
+    any_symbol(list_symbol list)
+      : content(std::move(list))
+    {}
+    any_symbol(symbol&& s)
+    {
+        s.visit_none([&](auto& obj)
+        {
+            content = std::move(obj);
+        });
+    }
+    any_symbol(const symbol& s)
+    {
+        s.visit_none([&](auto obj)
+        {
+            content = std::move(obj);
+        });
+    }
+private:
+    struct address_getter_type
+        : boost::static_visitor<>
+    {
+        typedef symbol* result_type;
+        template<class T>
+        symbol* operator()(T& obj) const
+        {
+            return &obj;
+        }
+    };
+public:
+    /*
+    symbol* operator&()
+    {
+        return boost::apply_visitor(address_getter_type(), content);
+    }
+    const symbol* operator&() const
+    {
+        return &(*const_cast<any_symbol*>(this));
+    }
+    */
+    operator symbol&()
+    {
+        return *boost::apply_visitor(address_getter_type(), content);
+    }
+    operator const symbol&() const
+    {
+        return *const_cast<any_symbol*>(this);
+    }
+/*
+    symbol::type_value type() const
+    {
+        return symbol::NONE;
+    }
+
+    lit_symbol& lit()
+    {
+        return static_cast<symbol&>(*this).lit();
+    }
+    const lit_symbol& lit() const
+    {
+        return static_cast<const symbol&>(*this).lit();
+    }
+    lit_symbol& lit_else(const std::exception& exc)
+    {
+        return static_cast<symbol&>(*this).lit_else(exc);
+    }
+    const lit_symbol& lit_else(const std::exception& exc) const
+    {
+        return static_cast<const symbol&>(*this).lit_else(exc);
+    }
+
+    ref_symbol& ref()
+    {
+        return static_cast<symbol&>(*this).ref();
+    }
+    const ref_symbol& ref() const
+    {
+        return static_cast<const symbol&>(*this).ref();
+    }
+    ref_symbol& ref_else(const std::exception& exc)
+    {
+        return static_cast<symbol&>(*this).ref_else(exc);
+    }
+    const ref_symbol& ref_else(const std::exception& exc) const
+    {
+        return static_cast<const symbol&>(*this).ref_else(exc);
+    }
+
+    list_symbol& list()
+    {
+        return static_cast<symbol&>(*this).list();
+    }
+    const list_symbol& list() const
+    {
+        return static_cast<const symbol&>(*this).list();
+    }
+    list_symbol& list_else(const std::exception& exc)
+    {
+        return static_cast<symbol&>(*this).list_else(exc);
+    }
+    const list_symbol& list_else(const std::exception& exc) const
+    {
+        return static_cast<const symbol&>(*this).list_else(exc);
+    }
+
+    template<class Visitor>
+    void visit_none(Visitor&& visitor)
+    {
+        static_cast<symbol&>(*this).visit_none(visitor);
+    }
+    template<class Visitor>
+    void visit_none(Visitor&& visitor) const
+    {
+        static_cast<const symbol&>(*this).visit_none(visitor);
+    }
+    template<class Visitor>
+    void visit(Visitor&& visitor)
+    {
+        static_cast<symbol&>(*this).visit(visitor);
+    }
+    template<class Visitor>
+    void visit(Visitor&& visitor) const
+    {
+        static_cast<const symbol&>(*this).visit(visitor);
+    }
+*/
+private:
+    boost::variant<none_symbol, lit_symbol, ref_symbol, list_symbol> content;
+};
+
+/*
+constexpr size_t constexpr_max_for_symbol(size_t a, size_t b)
+{
+    if(a > b)
+        return a;
+    else
+        return b;
+}
+constexpr size_t constexpr_max_for_symbol(size_t a)
+{
+    return a;
+}
+template <class... TS>
+constexpr size_t constexpr_max_for_symbol(size_t a, size_t b, TS... s)
+{
+    return constexpr_max_for_symbol(a, constexpr_max_for_symbol(b, s...));
+}
+
+constexpr const size_t max_symbol_size = constexpr_max_for_symbol(
+        sizeof(none_symbol), sizeof(lit_symbol), sizeof(ref_symbol), sizeof(list_symbol));
+
+class any_symbol
+  : public symbol
+{
+public:
+    any_symbol()
+    {
+        construct(none_symbol());
+    }
+    any_symbol(const symbol& that)
+    {
+        that.visit_none([&](auto obj)
+        {
+            construct(obj);
+        });
+    }
+    any_symbol(symbol&& that) noexcept
+    {
+        that.visit_none([&](auto& obj)
+        {
+            construct(std::move(obj));
+        });
+    }
+
+    any_symbol(lit_symbol literal)
+    {
+        construct(std::move(literal));
+    }
+    any_symbol(ref_symbol reference)
+    {
+        construct(std::move(reference));
+    }
+    any_symbol(list_symbol list)
+    {
+        construct(std::move(list));
+    }
+    ~any_symbol() noexcept
+    {
+        destruct();
+    }
+    
+    any_symbol& operator=(symbol&& that) noexcept
+    {
+        destruct();
+        that.visit([&](auto& obj)
+        {
+            construct(std::move(obj));
+        });
+        return *this;
+    }
+    any_symbol& operator=(const symbol& that)
+    {
+        that.visit([&](auto obj)
+        {
+            // obj has been copied already
+            destruct(); // noexcept
+            construct(std::move(obj)); // noexcept
+        });
+        return *this;
+    }
+private:
+    char buffer[max_symbol_size];
+    
+    template<class T>
+    void construct(T&& obj)
+    {
+        typedef typename std::decay<T>::type actual_type;
+        new(buffer) actual_type(std::forward<T>(obj));
+    }
+    void destruct() noexcept
+    {
+        visit_none([](auto& obj)
+        {
+            typedef typename std::decay<decltype(obj)>::type T;
+            obj.~T();
+        });
+    }
+};
+*/
+
+inline list_symbol::list_symbol(std::initializer_list<any_symbol> l)
+  : list_symbol(std::vector<any_symbol>{l.begin(), l.end()})
+{}
+inline any_symbol& list_symbol::operator[](size_t i)
+{
+    return v[i];
+}
+inline const any_symbol& list_symbol::operator[](size_t i) const
+{
+    return v[i];
+}
+inline void list_symbol::push_back(symbol&& s)
+{
+    v.push_back(std::move(s));
+}
+inline void list_symbol::push_back(const symbol& s)
+{
+    v.push_back(s);
+}
 
 #endif
 
--- test/symbol.cpp
+++ test/symbol.cpp
@@ -1,41 +1,169 @@
+#define BOOST_TEST_MODULE symbol
+#include <boost/test/included/unit_test.hpp>
+
+#include <algorithm>
+
+using std::equal;
+using std::begin;
+
 #include "symbol_building.hpp"
 
-
-int main()
+BOOST_AUTO_TEST_CASE(none_test)
 {
-    {
-        symbol a = lit("asdf");
-        symbol b = lit("asdf");
-        assert(a == b);
-    }
-    {
-        symbol a = list(lit("asdf"), sref("sdf"));
-        symbol b = list(lit("asdf"), sref("sdf"));
-        assert(a == b);
-    }
-    {
-        symbol a = list(lit("ff"), sref("fff"), list());
-        symbol b = list(lit("asdff"), sref("fff"), list());
-        assert(a != b);
-    }
-    {
-        symbol a = list(lit("asdff"), sref("fff"), list());
-        symbol b = list(lit("asdff"), sref("fff"), list());
-        
-        symbol c = sref("ff", &a);
-        symbol d = sref("ff", &b);
-        assert(c == d);
-        
-        a = list(lit("asdff"), sref(""), list());
-        assert(c != d);
-
-        c = sref("ff", &b);
-        assert(c == d);
-
-        c = sref("ffsdf", &b);
-        assert(c != d);
-
-        c = sref("ff");
-        assert(c != d);
-    }
+    none_symbol none1;
+    none_symbol none2;
+    BOOST_CHECK(none1 == none2);
+    BOOST_CHECK(!(none1 != none2));
+    BOOST_CHECK((symbol&)none1 == (symbol&)none2);
 }
+
+BOOST_AUTO_TEST_CASE(lit_test)
+{
+    lit_symbol lit1 = "abcde";
+    const lit_symbol lit2 = lit1;
+    lit_symbol lit3 = "asdff";
+    
+    BOOST_CHECK(std::equal(lit1.begin(), lit1.end(), "abcde"));
+    BOOST_CHECK(std::equal(lit2.begin(), lit2.end(), "abcde"));
+    BOOST_CHECK(std::equal(lit3.begin(), lit3.end(), "asdff"));
+
+    BOOST_CHECK(lit1[3] == 'd');
+    lit3[3] = 'g';
+    BOOST_CHECK(std::equal(lit3.begin(), lit3.end(), "asdgf"));
+
+    BOOST_CHECK(lit1 == lit2);
+    BOOST_CHECK(lit2 != lit3);
+}
+
+BOOST_AUTO_TEST_CASE(ref_test)
+{
+    ref_symbol ref1{"abcde"};
+    const ref_symbol ref2 = ref1;
+    ref_symbol ref3{"abcde", &ref1};
+    ref_symbol ref4{"asdff", &ref1};
+    
+    BOOST_CHECK(std::equal(ref1.begin(), ref1.end(), "abcde"));
+    BOOST_CHECK(std::equal(ref2.begin(), ref2.end(), "abcde"));
+    BOOST_CHECK(std::equal(ref3.begin(), ref3.end(), "abcde"));
+    BOOST_CHECK(std::equal(ref4.begin(), ref4.end(), "asdff"));
+
+    BOOST_CHECK(ref1[3] == 'd');
+    ref4[3] = 'g';
+    BOOST_CHECK(std::equal(ref4.begin(), ref4.end(), "asdgf"));
+
+    BOOST_CHECK(ref1 == ref2);
+    BOOST_CHECK(ref2 != ref3);
+    BOOST_CHECK(ref3 != ref4);
+}
+
+BOOST_AUTO_TEST_CASE(cast_test)
+{
+    symbol* s;
+
+    lit_symbol lit = "abcde";
+    s = &lit;
+    BOOST_CHECK_EQUAL(s->type(), symbol::LITERAL);
+    BOOST_CHECK_EQUAL(&s->lit(), &lit);
+
+    ref_symbol ref{"abcd"};
+    s = &ref;
+    BOOST_CHECK_EQUAL(s->type(), symbol::REFERENCE);
+    BOOST_CHECK_EQUAL(&s->ref(), &ref);
+
+    list_symbol list;
+    s = &list;
+    BOOST_CHECK_EQUAL(s->type(), symbol::LIST);
+    BOOST_CHECK_EQUAL(&s->list(), &list);
+}
+
+BOOST_AUTO_TEST_CASE(visit_test)
+{
+    bool visited;
+    symbol* s;
+    
+    lit_symbol lit = "abcde";
+    s = &lit;
+    visited = false;
+    s->visit([&](auto& obj)
+    {
+        visited = true;
+        BOOST_CHECK((void*)&obj == &lit);
+    });
+    BOOST_CHECK(visited);
+
+    ref_symbol ref{"asdf"};
+    s = &ref;
+    visited = false;
+    s->visit([&](auto& obj)
+    {
+        visited = true;
+        BOOST_CHECK((void*)&obj == &ref);
+    });
+    BOOST_CHECK(visited);
+    
+    list_symbol list;
+    s = &list;
+    visited = false;
+    s->visit_none([&](auto& obj)
+    {
+        visited = true;
+        BOOST_CHECK((void*)&obj == &list);
+    });
+    BOOST_CHECK(visited);
+}
+
+BOOST_AUTO_TEST_CASE(any_symbol_lit_ref_test) // only test any_symbol with lit_symbol and ref_symbol
+{
+    any_symbol none_any;
+    symbol& none_base = none_any;
+    BOOST_CHECK(none_base.type() == symbol::NONE);
+    BOOST_CHECK(none_any == none_symbol());
+    
+    lit_symbol lit_obj = "123";
+    any_symbol lit_any = lit_obj;
+    BOOST_CHECK(lit_obj == lit_any);
+    
+    symbol& lit_base = lit_any;
+    lit_base.visit_none([&](auto& obj)
+    {
+        BOOST_CHECK(obj.type() == symbol::LITERAL);
+        BOOST_CHECK(obj == lit_obj);
+    });
+
+    ref_symbol ref_obj{"123", &lit_obj};
+    any_symbol ref_any = ref_obj;
+    BOOST_CHECK(ref_obj == ref_any);
+
+    symbol& ref_base = ref_any;
+    ref_base.visit_none([&](auto& obj)
+    {
+        BOOST_CHECK(obj.type() == symbol::REFERENCE);
+        BOOST_CHECK(obj == ref_obj);
+    });
+}
+
+BOOST_AUTO_TEST_CASE(list_symbol_test)
+{
+    ref_symbol obj1{"abcde"};
+    lit_symbol obj2 = "abcde";
+    ref_symbol obj3{"abcde", &obj1};
+    list_symbol obj4 = {obj2, obj3};
+    
+    list_symbol list1 = {obj1, obj2, obj4};
+    list_symbol list2 = list1;
+    list_symbol list3 = {obj1, obj3, obj4};
+    
+    any_symbol expected1_2[] = {obj1, obj2, obj4};
+    BOOST_CHECK(equal(list1.begin(), list1.end(), begin(expected1_2)));
+    BOOST_CHECK(equal(list2.begin(), list2.end(), begin(expected1_2)));
+
+    any_symbol expected3[] = {obj1,obj3, obj4};
+    BOOST_CHECK(equal(list3.begin(), list3.end(), begin(expected3)));
+    list3[2] = obj2;
+    any_symbol expected3_now[] = {obj1, obj3, obj2};
+    BOOST_CHECK(equal(list3.begin(), list3.end(), begin(expected3_now)));
+
+    BOOST_CHECK(list1 == list2);
+    BOOST_CHECK(list2 != list3);
+}
+
