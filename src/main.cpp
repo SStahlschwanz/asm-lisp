1ce54e2b31d1a15d741d8da9e94cb7bfbccbfedd
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2015-02-22 14:37:25 +0100

    fix main

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,13 @@ int main()
     istream_iterator<char> begin{std::cin};
     istream_iterator<char> end{};
     
-    parse_state<istream_iterator<char>> state{begin, end, "command line"};
+    parse_state<istream_iterator<char>> state{begin, end, 1};
     
     try
     {
         parse_file(state);
     } catch(const parse_exception& exc)
     {
-        cerr << "command line:" << exc.pos.line << ":" << exc.pos.line_position 
-                << ": error: " << parse_error_strings[(unsigned int)exc.error] << endl;
+        cerr << "parse error" << endl;
     }
 }
