161e11af43c3209f253b48b1bb74c78e88a71351
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2015-03-02 15:47:28 +0100

    remove unnecessary newline

--- src/compile_macro.hpp
+++ src/compile_macro.hpp
@@ -4,8 +4,7 @@
 #include "symbol.hpp"
 #include "compilation_context.hpp"
 
-macro_symbol compile_macro(list_symbol::const_iterator begin, list_symbol::const_iterator end,
-        compilation_context& context);
+macro_symbol compile_macro(list_symbol::const_iterator begin, list_symbol::const_iterator end, compilation_context& context);
 
 #endif
 
